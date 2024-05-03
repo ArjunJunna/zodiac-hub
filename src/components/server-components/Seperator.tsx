d001aaf127d6501463d7e5598e21ebca20b6bca1
Arjun Junna <arjun9852@gmail.com>
2024-05-03 12:12:22 +0530

    style : update seperator in darkmode

--- src/components/server-components/Seperator.tsx
+++ src/components/server-components/Seperator.tsx
@@ -1,7 +1,7 @@
 const Seperator = () => {
   return (
     <div className="px-3">
-      <div className="dark:bg-gray-100/40 bg-gray-200 h-[0.01rem] my-2"></div>
+      <div className="dark:bg-slate-500/30 bg-gray-200 h-[0.01rem] my-2"></div>
     </div>
   );
 };
