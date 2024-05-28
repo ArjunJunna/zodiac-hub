a396c8b15e039b52df457115885c5ca6b6847426
Arjun Junna <arjun9852@gmail.com>
2024-05-28 12:48:32 +0530

    feat : add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+import { BASE_URL, PUBLIC_BASE_URL } from "@/lib/Constants";
+import { ForumType,PostType } from "@/utils/types";
+
+export default async function sitemap() {
+  const postsResponse = await fetch(`${BASE_URL}/posts`);
+  const posts: PostType[] = await postsResponse.json();
+
+  const forumsResponse = await fetch(`${BASE_URL}/forums`);
+  const forums: ForumType[] = await forumsResponse.json();
+
+  const postEntries: MetadataRoute.Sitemap = posts.map(({ id }) => ({
+    url: `${PUBLIC_BASE_URL}/post/${id}`,
+  }));
+
+  const forumEntries: MetadataRoute.Sitemap = forums.map(({ id }) => ({
+    url: `${PUBLIC_BASE_URL}/forums/${id}`,
+  }));
+
+  return [...postEntries, ...forumEntries];
+}
--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = "https://zodiac-hub.onrender.com/api/v1";
+
+export const PUBLIC_BASE_URL = "http://localhost:3000";
