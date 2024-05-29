be078cce54c6cdd4beb3b68a4cc515f4d2f6ad0f
Arjun Junna <arjun9852@gmail.com>
2024-05-29 14:18:02 +0530

    refactor : middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/z/:path*", "/post/:path*", "/profile"] };
+export const config = { matcher: ["/z/:path*", "/profile"] };
