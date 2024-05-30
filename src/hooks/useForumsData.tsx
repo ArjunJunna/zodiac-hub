1150b7a3ad2c52eb79ddcc41c54c72783df8fe7f
Arjun Junna <arjun9852@gmail.com>
2024-05-30 17:55:49 +0530

    refactor : useForumsData hook

--- src/hooks/useForumsData.tsx
+++ src/hooks/useForumsData.tsx
@@ -3,7 +3,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { publicRequest } from "../requestMethods";
 import { ForumType } from "@/utils/types";
-import axios from "axios";
 
 const useForumsData = () => {
   const fetchForumsData = async (): Promise<ForumType[]> => {
@@ -25,31 +24,4 @@ const useForumsData = () => {
   };
 };
 
-const useForumByIdData = (forumId: string) => {
-  const fetchForumById = async ({ queryKey }: { queryKey: any }) => {
-    try {
-      const [_, forumId] = queryKey;
-      const response = await axios.get(
-        `https://zodiac-hub.onrender.com/api/v1/forums/${forumId}`
-      );
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const { data, isLoading, isError, error, isFetching } = useQuery({
-    queryKey: ["forum", forumId],
-    queryFn: fetchForumById,
-  });
-
-  return {
-    data,
-    isLoading,
-    isError,
-    error,
-    isFetching,
-  };
-};
-
-export { useForumsData, useForumByIdData };
+export { useForumsData};
