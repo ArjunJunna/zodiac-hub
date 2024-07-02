914c3f1237977c0633336c2e27234c4a07a5b002
Arjun Junna <arjun9852@gmail.com>
2024-07-03 00:00:39 +0530

    fix : sitemap url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import { BASE_URL, PUBLIC_BASE_URL } from '@/lib/Constants';
+import { BASE_URL } from '@/lib/Constants';
 import { ForumType, PostType } from '@/utils/types';
 
 export default async function sitemap() {
@@ -10,11 +10,11 @@ export default async function sitemap() {
   const forums: ForumType[] = await forumsResponse.json();
 
   const postEntries: MetadataRoute.Sitemap = posts.map(({ id }) => ({
-    url: `${PUBLIC_BASE_URL}/post/${id}`,
+    url: `${BASE_URL}/post/${id}`,
   }));
 
   const forumEntries: MetadataRoute.Sitemap = forums.map(({ id }) => ({
-    url: `${PUBLIC_BASE_URL}/forums/${id}`,
+    url: `${BASE_URL}/forums/${id}`,
   }));
 
   return [...postEntries, ...forumEntries];
